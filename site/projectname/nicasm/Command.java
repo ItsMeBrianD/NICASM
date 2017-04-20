e54d63dc40a183079020b336f44b7c4c2a69e402
Brian <bmdonald1998@gmail.com>
2017-04-20 06:32:27 -0600

    Fixed Commands
    Several Commands had incorrect Regular Expressions, this has been fixed.

--- site/projectname/nicasm/Command.java
+++ site/projectname/nicasm/Command.java
@@ -5,23 +5,24 @@ import static site.projectname.nicasm.REGEX.*;
 public enum Command {
     ADD ("ADD",  "(ADD)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+REGISTER+"|"+IMM5+")",  "0001", "DR SR1 XXX SR2 "),
     AND ("AND",  "(AND)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+REGISTER+"|"+IMM5+")",  "0101", "DR SR1 XXX SR2 "),
-    BR  ("BR",   "((BR)[Z]?[N]?[P]?)([\\s]+)"+LABEL,                                          "0000", "XXX XXXXXXXXX  "),
+    BR  ("BR",   "((BR)[Zz]?[Nn]?[Pp]?)([\\s]+)"+LABEL,                                     "0000", "XXX XXXXXXXXX  "),
     JMP ("JMP",  "(JMP)([\\s]+)" +REGISTER,                                                 "1100", "000 BR 000000  "),
     JSR ("JSR",  "(JSR)([\\s]+)" +LABEL,                                                    "0100", "1 XXXXXXXXXXX  "),
     JSRR("JSRR", "(JSRR)([\\s]+)"+REGISTER,                                                 "0100", "000 BR 000000  "),
-    LD  ("LD",   "(LD)([\\s]+)"  +REGISTER+SPACE+LABEL,                                     "0010", "DR XXXXXXXXX   "),
-    LDI ("LDI",  "(LDI)([\\s]+)" +REGISTER+SPACE+LABEL,                                     "1010", "DR XXXXXXXXX   "),
-    LDR ("LDR",  "(LDR)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+LABEL+"|"+IMM6+")",     "0110", "DR BR XXXXXX   "),
-    LEA ("LEA",  "(LEA)([\\s]+)" +REGISTER+SPACE+LABEL,                                     "1110", "DR XXXXXXXXX   "),
+    LD  ("LD",   "(LD)([\\s]+)"  +REGISTER+SPACE+VARIABLE,                                  "0010", "DR XXXXXXXXX   "),
+    LDI ("LDI",  "(LDI)([\\s]+)" +REGISTER+SPACE+VARIABLE,                                  "1010", "DR XXXXXXXXX   "),
+    LDR ("LDR",  "(LDR)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+VARIABLE+"|"+IMM6+")",  "0110", "DR BR XXXXXX   "),
+    LEA ("LEA",  "(LEA)([\\s]+)" +REGISTER+SPACE+VARIABLE,                                  "1110", "DR XXXXXXXXX   "),
     NOT ("NOT",  "(NOT)([\\s]+)" +REGISTER+SPACE+REGISTER,                                  "1001", "DR SR 111111   "),
     RET ("RET",  "(RET)([\\s]*)",                                                           "1100", "000111000000   "),
     RTI ("RTI",  "(RTI)([\\s]*)",                                                           "1000", "000000000000   "),
-    ST  ("ST",   "(ST)([\\s]+)"  +REGISTER+SPACE+LABEL,                                     "0011", "SR XXXXXXXXX   "),
-    STI ("STI",  "(STI)([\\s]+)" +REGISTER+SPACE+LABEL,                                     "1011", "SR XXXXXXXXX   "),
-    STR ("STR",  "(STR)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+LABEL+"|"+IMM6+")",     "0111", "SR BR XXXXXX   "),
+    ST  ("ST",   "(ST)([\\s]+)"  +REGISTER+SPACE+VARIABLE,                                  "0011", "SR XXXXXXXXX   "),
+    STI ("STI",  "(STI)([\\s]+)" +REGISTER+SPACE+VARIABLE,                                  "1011", "SR XXXXXXXXX   "),
+    STR ("STR",  "(STR)([\\s]+)" +REGISTER+SPACE+REGISTER+SPACE+"("+VARIABLE+"|"+IMM6+")",  "0111", "SR BR XXXXXX   "),
     TRAP("TRAP", "(TRAP)([\\s]+)("+LABEL +"|"+IMM8+")",                                     "1111", "0000 XXXXXXXX  "),
 
-    FILL(".FILL",  REGEX.FILL.toString(),                                                   "XXXX", "XXXXXXXXXXXXXXXX");
+    FILL(".FILL",  REGEX.FILL.toString(),                                                   "XXXX", "XXXXXXXXXXXXXXXX"),
+    BLK (".BLK",   REGEX.BLK.toString(),                                                    "XXXX", "XXXXXXXXXXXXXXXX");
     public final String regex;
     public final String firstFour;
     public final String syntax;
