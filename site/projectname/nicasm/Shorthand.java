03873e2997a95ec88356ffd32d82db43ba0df13d
Brian <bmdonald1998@gmail.com>
2017-04-21 20:08:08 -0600

    Added commands and made syntax more flexibile

--- site/projectname/nicasm/Shorthand.java
+++ site/projectname/nicasm/Shorthand.java
@@ -1,10 +1,13 @@
 package site.projectname.nicasm;
 
+import java.util.HashMap;
+
 import static site.projectname.nicasm.REGEX.*;
 import site.projectname.err.SyntaxErrorException;
 import site.projectname.util.Logger;
 
 public enum Shorthand{
+	// Math
 	SUB(".SUB","(.SUB)([\\s]+)"+REGISTER+SPACE+REGISTER+SPACE+"("+REGISTER+"|"+IMM5+")",
 		".SUB DR SR1 SR2",
 		new String[]{
@@ -20,13 +23,75 @@ public enum Shorthand{
 			"BRP #-2"
 		}
 	),
-	PUSH("PUSH","(PUSH)([\\s]+)"+REGISTER+("[\\s]*"),
-		"PUSH DR",
+	// Stack
+	INIT(".INIT","(.INIT)",
+		".INIT",
 		new String[]{
-			"ADD R5,R5,#1",
-			"STR DR,R5,#0"
+			"LD R5,$STACK",
+			"LD R6,$STACK",
+			"ADD R6,R6,#1"
 		}
-	);
+	),
+	PUSH(".PUSH","(.PUSH)([\\s]+)"+REGISTER,
+		".PUSH DR",
+		new String[]{
+			"ADD R6,R6,#1",
+			"STR DR,R6,#0"
+		}
+	),
+	GETV(".GETV","(.GETV)([\\s]+)"+REGISTER+SPACE+IMM5,
+		".GETV DR IMM5",
+		new String[]{
+			"LDR DR,R6,IMM5"
+		}
+
+	),
+	SETV(".SETV","(.SETV)([\\s]+)"+IMM5+SPACE+REGISTER,
+		".SETV IMM5 DR",
+		new String[]{
+			"STR DR,R6,IMM5"
+		}
+	),
+	GETP(".GETP","(.GETP)([\\s]+)"+REGISTER+SPACE+IMM5,
+		".GETP DR IMM5",
+		new String[]{
+			"ADD R5,R5,#-3",
+			"LDR DR,R5,IMM5",
+			"ADD R5,R5,#3"
+		}
+	),
+	SETP(".SETP","(.SETP)([\\s]+)"+IMM5+SPACE+REGISTER,
+		".SETP IMM5 DR",
+		new String[]{
+			"ADD R5,R5,#-3",
+			"STR DR,R5,IMM5",
+			"ADD R5,R5,#3"
+		}
+	),
+	RETURN(".RETURN","(RETURN)([\\s]+)"+REGISTER,
+		".RETURN DR",
+		new String[]{
+			"COPY R6,R5", // R6 = R5
+			"POP R5",
+			"RET",
+			"POP R7",
+			"POP DR",
+			"COPY R6,R5",
+			"ADD R6,R6,#1"
+		}
+	),
+	// Utility
+	COPY(".COPY","(.COPY)([\\s]+)"+REGISTER+SPACE+REGISTER,
+		".COPY DR,SR1",
+		new String[]{
+			"ADD DR,SR1,#0"
+		}
+	),
+	ZERO(".ZERO","(.ZERO)[\\s]+"+REGISTER,
+		".ZERO R",
+		new String[]{
+			"AND R,R,#0"
+		});
 	public final String value;
 	public final String regex;
 	public final String syntax;
@@ -62,14 +127,19 @@ public enum Shorthand{
 			throw new SyntaxErrorException(in,this.regex,lineAddr);
 		String[] parts = in.replace(","," ").replace("[\\s]+"," ").split(" ");
 		String[] converted = new String[this.output.length];
-		int i = 0;
+		int i = 1;
+		HashMap<String,String> map = new HashMap<String,String>();
+		for(String key: syntax.replace(","," ").split(" ")){
+			if(!key.equals(value))
+				map.put(key,parts[i++]);
+		}
+		i = 0;
 		for(String out: output){
-			out = out.replaceAll("DR",parts[1]);
-			if(parts.length>2)
-				out = out.replaceAll("SR1",parts[2]);
-			if(parts.length>3)
-				out = out.replaceAll("SR2",parts[3]);
-			converted[i++] = out;
+			String p = out.replace(",[\\s]+",",").split(" ")[1];
+			out = out.replace(",[\\s]+",",").split(" ")[0];
+			for(String key: map.keySet())
+				p = p.replaceAll(key,map.get(key));
+			converted[i++] = out +" "+p;
 		}
 		return converted;
 	}
